79af2f433a306c3378816f507575db794bd19630
Ho Van Khon <khonhv@dtp-education.com>
2023-12-11 15:58:16 +0700

    LicenseKey

---
+++ LicenseKey/frmAbout.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace LicenseKey
+{
+    partial class frmAbout
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblProductName = new System.Windows.Forms.Label();
+            this.lblProductID = new System.Windows.Forms.Label();
+            this.lblProductKey = new System.Windows.Forms.Label();
+            this.lblLicenseType = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(82, 152);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Product Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Product ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Product Key:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(44, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "License Type:";
+            // 
+            // lblProductName
+            // 
+            this.lblProductName.AutoSize = true;
+            this.lblProductName.Location = new System.Drawing.Point(125, 35);
+            this.lblProductName.Name = "lblProductName";
+            this.lblProductName.Size = new System.Drawing.Size(13, 13);
+            this.lblProductName.TabIndex = 2;
+            this.lblProductName.Text = "?";
+            // 
+            // lblProductID
+            // 
+            this.lblProductID.AutoSize = true;
+            this.lblProductID.Location = new System.Drawing.Point(125, 61);
+            this.lblProductID.Name = "lblProductID";
+            this.lblProductID.Size = new System.Drawing.Size(13, 13);
+            this.lblProductID.TabIndex = 2;
+            this.lblProductID.Text = "?";
+            // 
+            // lblProductKey
+            // 
+            this.lblProductKey.AutoSize = true;
+            this.lblProductKey.Location = new System.Drawing.Point(125, 86);
+            this.lblProductKey.Name = "lblProductKey";
+            this.lblProductKey.Size = new System.Drawing.Size(13, 13);
+            this.lblProductKey.TabIndex = 2;
+            this.lblProductKey.Text = "?";
+            // 
+            // lblLicenseType
+            // 
+            this.lblLicenseType.AutoSize = true;
+            this.lblLicenseType.Location = new System.Drawing.Point(125, 110);
+            this.lblLicenseType.Name = "lblLicenseType";
+            this.lblLicenseType.Size = new System.Drawing.Size(13, 13);
+            this.lblLicenseType.TabIndex = 2;
+            this.lblLicenseType.Text = "?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(178, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmAbout
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 187);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblLicenseType);
+            this.Controls.Add(this.lblProductKey);
+            this.Controls.Add(this.lblProductID);
+            this.Controls.Add(this.lblProductName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOK);
+            this.MaximizeBox = false;
+            this.Name = "frmAbout";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "About";
+            this.Load += new System.EventHandler(this.frmAbout_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblProductName;
+        private System.Windows.Forms.Label lblProductID;
+        private System.Windows.Forms.Label lblProductKey;
+        private System.Windows.Forms.Label lblLicenseType;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ LicenseKey/frmAbout.cs
@@ -0,0 +1,56 @@
+﻿using FoxLearn.License;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LicenseKey
+{
+    public partial class frmAbout : Form
+    {
+        public frmAbout()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        const int ProductCode = 1;
+
+        private void frmAbout_Load(object sender, EventArgs e)
+        {
+            lblProductID.Text = ComputerInfo.GetComputerId();
+            //KeyManager km = new KeyManager(lblProductID.Text);
+            KeyManager km = new KeyManager("A60A-DA4B-C671-118D-2DB8-4472-6F93-1CF4-BD74-36EA-476D-CF82-870D-DA5F-1598-D068-B3BA-A1D0-952D-F01F-BBCE-66EA-BCB4-CFAC");
+            LicenseInfo lic = new LicenseInfo();
+            int value = km.LoadSuretyFile(string.Format(@"{0}\Key.lic", Application.StartupPath), ref lic);
+            string productKey = lic.ProductKey;
+            if(km.ValidKey(ref productKey))
+            {
+                KeyValuesClass kv = new KeyValuesClass();
+                if(km.DisassembleKey(productKey, ref kv))
+                {
+                    lblProductName.Text = "dolathanchuong";
+                    lblProductKey.Text = productKey;
+                    if (kv.Type == LicenseType.TRIAL)
+                        lblLicenseType.Text = string.Format("{0} days", (kv.Expiration - DateTime.Now.Date).Days);
+                    else
+                        lblLicenseType.Text = "Full";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
